8998eeb8032fd760e35f2feb7a0ecc5fee76667f
Javier Medina <jhmedina@tsc.gob.hn>
2024-06-21 16:09:40 -0600

    21 de jumio

---
+++ WebServicesLayer/Util/Util.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.Configuration;
+using System.Net.Http;
+namespace WebServicesLayer.Util
+{
+    public class Util
+    {
+        #region servicios TSC FILES UTL
+        public static string obtenerDataAchivos = "Data/";
+        public static string obtenerAchivos = "Download/";
+        public static string subirAchivos = "Upload";
+        #endregion
+        #region métodos reutilizables para el llamado de servicios
+        public static HttpClient getGoFilesUtlHeaders()
+        {
+            HttpClient _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("Username", ConfigurationManager.AppSettings["GoFilesUtlUser"].ToString());
+            _httpClient.DefaultRequestHeaders.Add("Password", ConfigurationManager.AppSettings["GoFilesUtlPassword"].ToString());
+            _httpClient.DefaultRequestHeaders.Add("File_Path", ConfigurationManager.AppSettings["GoFilesUtlPath"].ToString());
+            //_httpClient.DefaultRequestHeaders.Add("Path", ConfigurationManager.AppSettings["GoFilesUtlConnString"].ToString());
+            return _httpClient;
+        }
+        public static HttpClient getServiceClient(string token)
+        {
+            HttpClient _httpClient = new HttpClient();
+
+
+            _httpClient.DefaultRequestHeaders.Add("Authorization", token);
+            return _httpClient;
+        }
+
+        public static string GetFinalUrl(string url)
+        {
+            return ConfigurationManager.AppSettings["urlApiHis"].ToString() + url;
+        }
+
+        public static string GetFinalGoFilesUtlUrl(string url)
+        {
+            return ConfigurationManager.AppSettings["UrlGoFilesUtlSASI"].ToString() + url;
+        }
+        #endregion
+    }
+}
---
+++ WebServicesLayer/Util/UtilClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+using WebServicesLayer.Models;
+using System.Web;
+
+using System.IO;
+
+
+namespace WebServicesLayer.Util
+{
+    public static class UtilClass
+    {
+        public static String alertNotification = "AlertNotification";
+
+        public static string estado = "0";
+        public static int codigoError = 2;
+        public static int codigoExitoso = 0;
+
+        public static int tipoOperacionCrear = 1;
+        public static int tipoOperacionModificar = 2;
+        public static int tipoOperacionEliminar = 3;
+
+        public static int tipoEmpleadoPermanente = 1;
+        public static int tipoEmpleadoContrato = 2;
+        public static int tipoEmpleadoPrueba = 3;
+        public static int tipoEmpleadoInterino = 4;
+        public static int tipoEmpleadoPractica = 5;
+        public static int tipoEmpleadoExterno = 6;
+
+        public static string tipoSentenciaInsert = "INSERT";
+        public static string tipoSentenciaUpdate = "UPDATE";
+        public static string tipoSentenciaDelete = "DELETE";
+
+        public static string flexFieldKeyDocumento = "CODIGO_DOCUMENTO";
+        public static int fileIdDocumentoSeguimiento = 1;
+
+
+        //public static string GetSiteName()
+        //{
+        //    string SiteName = "";
+
+        //    if (!HttpContext.Current.Request.Url.Host.Contains("localhost"))
+        //    {
+        //        SiteName = HttpContext.Current.Request.ApplicationPath;
+        //    }
+        //    return SiteName;
+        //}
+
+        //public static string GetFinalUrl(string url, int codigoUsuario)
+        //{
+        //    string urlFinal = Database.Environment.GetFinalUrl(url, codigoUsuario);
+        //    return urlFinal;
+        //}
+
+        //public static UsuarioSesion GetUsuarioSesion()
+        //{
+        //    return (UsuarioSesion)HttpContext.Current.Session["USUARIO_SESION"];
+        //}
+
+        public static int maximoVarchar = 32500;
+        public static bool isEstadoActivo = true;
+        public static int EstadoActivo = 1;
+        public static int maximoInt = 32;
+        private static readonly Encoding encoding = Encoding.UTF8;
+
+
+        ///<summary>
+        /// Este método sirve encriptar las credenciales
+        ///</summary>
+        ///<remarks>
+        ///Autor: by lgarcia 20230911
+        ///</remarks>
+        ///<returns>
+        /// string
+        /// </returns>
+        public static string EncriptarPassword(string str)
+        {
+            string Key = "@PlTyvh4536#567*{KLa123AA!]o)_?K";
+            string encryptedText;
+            try
+            {
+                RijndaelManaged aes = new RijndaelManaged();
+                aes.KeySize = 256;
+                aes.BlockSize = 128;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.ECB;
+
+                aes.Key = encoding.GetBytes(Key);
+                aes.GenerateIV();
+
+                ICryptoTransform AESEncrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+                byte[] buffer = encoding.GetBytes(str);
+
+                encryptedText = Convert.ToBase64String(AESEncrypt.TransformFinalBlock(buffer, 0, buffer.Length));
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error: {e.Message}, Fuente: {e.Source}");
+            }
+            return encryptedText;
+        }
+
+    }
+
+}
